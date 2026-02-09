d20411bff7a4b2a2547bf54d9f4b77c355935df8
Taha Amine <tahamiinee@gmail.com>
2026-02-09 18:01:36 +0100

    fixing a the ouput pattern

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 console.log('Hi there')
 let arr = [1 , 2 , 3]
 arr.forEach((element, index) =>{
-    console.log('Index   Value ')
-    console.log(`  ${element} :    ${index}`)
+    console.log(`${element} : ${index}`)
 } )
